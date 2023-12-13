531880b48aedd070c1dcb168861a1cf126589403
HudaifDaif <107198955+HudaifDaif@users.noreply.github.com>
2023-12-13 16:15:57 +0000

    refactor toolbar

--- src/components/ToolBar/ToolBar.jsx
+++ src/components/ToolBar/ToolBar.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { getTopics } from "../../../utils/api.topics";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import "./Toolbar.css";
 
 const ToolBar = () => {
-	const [hasFilter, setHasFilter] = useState(false);
+	const [hasToolbar, setHasToolbar] = useState(false);
 	const [topics, setTopics] = useState([]);
 	const navigate = useNavigate();
+
+	let { topic } = useParams();
 
 	useEffect(() => {
 		getTopics().then((topics) => {
@@ -14,34 +16,37 @@ const ToolBar = () => {
 		});
 	}, []);
 
-	const handleFilter = () => {
-		setHasFilter((current) => !current);
+	const toggleToolbar = () => {
+		setHasToolbar((current) => !current);
 	};
 
 	const handleNavToFilter = (e) => {
 		e.preventDefault();
-		const topicQuery = e.target.elements.topicFilter.value;
-		topicQuery && navigate(`/topics/${topicQuery}`);
+		const topicQuery = e.target.value;
+		topicQuery ? navigate(`/topics/${topicQuery}`) : navigate("/");
 	};
 
 	return (
 		<section className="toolbar">
-			<button onClick={handleFilter}>filter</button>
-			<form onSubmit={handleNavToFilter}>
-				{hasFilter && (
-					<>
-						<select name="topicFilter" id="topicFilter">
-							<option value="">Select Filter</option>
-							{topics.map((topic) => (
-								<option key={topic.slug} value={topic.slug}>
-									{topic.slug}
-								</option>
-							))}
-						</select>
-						<button>Apply</button>
-					</>
-				)}
-			</form>
+			<button onClick={toggleToolbar}>Tools</button>
+			{hasToolbar && (
+				<>
+					<select
+						name="topicFilter"
+						id="topicFilter"
+						onChange={handleNavToFilter}
+					>
+						<option value="">
+							{topic ? "All Topics" : "Select Topic"}
+						</option>
+						{topics.map((topic) => (
+							<option key={topic.slug} value={topic.slug}>
+								{topic.slug}
+							</option>
+						))}
+					</select>
+				</>
+			)}
 		</section>
 	);
 };
