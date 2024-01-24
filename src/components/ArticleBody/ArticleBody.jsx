1826e0dd27e64ac4c032b51108055a492f8d23da
HudaifDaif <107198955+HudaifDaif@users.noreply.github.com>
2024-01-24 14:01:09 +0000

    fix voting on article for user

--- src/components/ArticleBody/ArticleBody.jsx
+++ src/components/ArticleBody/ArticleBody.jsx
@@ -2,14 +2,10 @@ import "./ArticleBody.css";
 
 import { UserContext } from "../../Contexts/User";
 import { patchArticleById } from "../../../utils/api.articles";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useState } from "react";
 
-const ArticleBody = ({ articleContents, commentsData }) => {
-	const [userVote, setUserVote] = useState(0);
-	const [voteError, setVoteError] = useState(null);
-	const { user } = useContext(UserContext);
-	
+const ArticleBody = ({ articleContents }) => {
 	const {
 		title,
 		article_img_url,
@@ -19,20 +15,37 @@ const ArticleBody = ({ articleContents, commentsData }) => {
 		topic,
 		votes,
 		article_id,
+		userVotes,
 	} = articleContents;
-	
-	const handleVote = (opinion, e) => {
-		const { parentElement } = e.target;
-		const voteButtons = parentElement.querySelectorAll(".vote-button");
 
-		if (!userVote || userVote !== opinion) {
-			voteButtons.forEach((button) => {
-				button.classList.remove("pressed-button");
-			});
+	const [userVote, setUserVote] = useState(0);
+	const [voteError, setVoteError] = useState(null);
+	const [displayedVotes, setDisplayedVotes] = useState(votes);
+	const { user } = useContext(UserContext);
+
+	useEffect(() => {
+		const storedUserVote = userVotes.find(
+			(vote) => vote.username === user.username
+		);
+		storedUserVote && setUserVote(storedUserVote.vote_value);
+		storedUserVote && setDisplayedVotes(storedUserVote.vote_value);
+	}, [user]);
+
+	const handleVote = (opinion) => {
+		if (opinion === userVote) {
+			patchArticleById(article_id, 0, user.username)
+				.then(() => {
+					setUserVote(0);
+					setDisplayedVotes((curr) => curr - opinion);
+				})
+				.catch(handleVoteError);
+		} else if (userVote !== opinion) {
 			patchArticleById(article_id, opinion, user.username)
 				.then(() => {
 					setUserVote(opinion);
-					e.target.classList.add("pressed-button");
+					userVote === 0
+						? setDisplayedVotes((curr) => curr + opinion)
+						: setDisplayedVotes((curr) => curr + opinion * 2);
 				})
 				.catch(handleVoteError);
 		}
@@ -60,18 +73,26 @@ const ArticleBody = ({ articleContents, commentsData }) => {
 					<p>Author: {author}</p>
 				</article>
 				<p>Comments: {comment_count}</p>
-				<p>Votes: {userVote ? votes + userVote : votes}</p>
+				<p>Votes: {displayedVotes}</p>
 				{user.username && (
 					<>
 						<button
-							onClick={(e) => handleVote(1, e)}
-							className="vote-button button"
+							onClick={() => handleVote(1)}
+							className={
+								userVote === 0 || userVote === -1
+									? "vote-button button"
+									: "vote-button button pressed-button"
+							}
 						>
 							+
 						</button>
 						<button
-							onClick={(e) => handleVote(-1, e)}
-							className="vote-button button"
+							onClick={() => handleVote(-1)}
+							className={
+								userVote === 0 || userVote === 1
+									? "vote-button button"
+									: "vote-button button pressed-button"
+							}
 						>
 							-
 						</button>
